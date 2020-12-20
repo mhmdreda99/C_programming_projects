4ccedb6f98aefbdcbadeef916e504bd18098ddba
mhmdreda99 <Moreda491999@gmail.com>
2020-12-21 00:33:11 +0200

    Reverse numbers

---
+++ C_practices/ReverseNumbers.c
@@ -0,0 +1,34 @@
+/**
+ * @file ReverseNumbers.c
+ * @author Mohamed Reda
+ * @brief progrm that Reverse any number entered from user
+ * @version 0.1
+ * @date 2020-12-21
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    unsigned int num;
+    int rev_num= 0 ,reminder,temp;
+    printf("Please Enter a Number :");
+    scanf("%d",&num);
+   
+
+    while (num!=0)
+    {
+        reminder=num %10 ;
+        // rev_number equation 
+        rev_num=rev_num*10 +reminder;
+        num/=10 ;
+
+    }
+    
+    printf("Reverse number is  is =%d",rev_num);
+
+    return 0 ;
+}
