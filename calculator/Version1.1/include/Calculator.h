0aca03263c7688f9961f674824aa0b66e5e578a8
mhmdreda99 <Moreda491999@gmail.com>
2020-12-28 16:30:24 +0200

    last of all

---
+++ calculator/Version1.1/include/Calculator.h
@@ -0,0 +1,23 @@
+/*
+ * Calculator.h
+ *
+ *  Created on: Dec 28, 2020
+ *      Author: Mohamed Reda
+ */
+
+#ifndef CALCULATOR_H_
+#define CALCULATOR_H_
+/*****************************************************************************
+*                               COMMON HEADER FILE
+*
+*****************************************************************************/
+#include "User_interface.h"
+
+/*****************************************************************************
+*                               FUNCTIONS PROTOTYPES
+*
+*****************************************************************************/
+ void Calc_App(void); // calculation application called by main app.
+
+
+#endif /* CALCULATOR_H_ */
